cf5fb582a1fb942409d8b16f7d64f2e0281f0a40
Amadou Toure <amadouelcapotoure@gmail.com>
2025-03-24 06:05:10 -0400

    corrected code

---
+++ build/default/debug/ln.o.d
@@ -0,0 +1,3 @@
+build/default/debug/ln.o: \
+ /Applications/microchip/mplabx/v6.20/packs/Microchip/dsPIC30F_DFP/1.5.254/xc16/support/../support/dsPIC30F/inc/p30f4013.inc \
+ ln.s
--- source.s
+++ source.s
@@ -3,11 +3,11 @@
 ;******************************************************************************
 ;    Filename            :  In_ADC.s  
 ;                                                                             *
-;    Author              :  Alain Royal                                       *
+;    Author              :  Amadou Toure                                      *
 ;    Company             :  Institut Teccart                                  *
 ;                                        *
-;    Date                :  26/10/2006                                        *
-;    File Version        :  1.20                                              *
+;    Date                :  20/03/2025                                        *
+;    File Version        :  1.00                                             *
 ;                                                                             *
 ;    Files Required: p30F6014.gld, p30f6014.inc				      *
 ;    Fichiers nÈcessaires:
@@ -24,8 +24,7 @@
 ;																			  *
 ;******************************************************************************
 ; Description:                                                        		 
-;   Création d'un sinus ? l'aide d'une table de données en mémoire programme  *
-;	avec le DCI et le Codec SI3021						
+;  sous programme permetant d'effectuer l'operation ln(1+x)		      *		
 ;                                                                             *
 ;******************************************************************************
 
@@ -76,6 +75,7 @@ Reserved: 	.space 0x24	; Espaces reservÈs
  _n: .space 2;
  _N: .space 2;
  _number: .space 2;
+    
 
 
 ;------------------------------------------------------------------------------
@@ -98,117 +98,135 @@ call _INIT_LN
   
 
 main: 
-	
 	call _ln_1_PLUS_x
-	;sftac A,#-15
-	;call _exposant
-	;call _test
-	;call _div_test
-	
 loop:
         BRA  loop            
 
-
 ;===============================================================================
 ;===============================================================================
-;Subroutine exposant: returns the result of _x^_n and stores the result in w12
+;Subroutine exposant: returns the result of _number^_n and stores the result in
+;w12 and w0
 ;...............................................................................
-_div_test:
-    mov #2,w6
-    mov #-1,w2
-    repeat #17
-    divf w2,w6
-    mov w0,w5
-    do #7,_end_div_test
-    mac w5*w6,A
-    inc w6,w6
-    
- _end_div_test:
-    nop
-    sftac A,#-15
-    return
-    
-_exposant:;add the value in _number and the exposant in _n, the result is in w12 and w0
+
+_exposant:;add the value in _number and the exposant in _n, the result is
+	  ;in w12 and w0
 	mov _n,w2
 	cp w2,#1
-	bra z,_n_is_1
+	bra z,_if_n_is_1
 	cp0 w2
-	bra z,_n_is_null
+	bra z,_else_if_n_is_null
 	dec2 w2,w2 
 	mov _number ,w1 
 	mov w1,w0
-	do w2,_exit
+	do w2,_exit_exposant
 	MUL.SS w0,w1,w12
 	mov w12,w0
 
-_exit: 
-    NOP
+_exit_exposant: 
+      NOP
     return
-_n_is_1:
-    mov _x,w12
+_if_n_is_1:
+    mov _number,w12
     mov w12,w0
     nop
     return
-_n_is_null:
+_else_if_n_is_null:
     mov #1,w12
     mov w12,w0
     nop
     return
+
 ;-------------------------------------------------------------------------------
 ;===============================================================================
 ;Subroutine _ln_1_PLUS_x: returns the result of ln(1+x):
+;the subroutine is subdivided in 4 parts
 ;..............................................................................
 _ln_1_PLUS_x:
-    
-    mov #-1,w2
     mov _N,w3
     dec w3,w3
-    do w3,_exit_ln
-;--------------------------part_1:(-1/_n)---------------------------------------
+    do w3,_exit_ln ;start of the loop
+_part_1:;(-1^_n+1)--------------------------------------------------------------
+    mov _n,w0
+    inc w0,w0
+    mov w0,_n
+    call _minus_1_exponent_n 
+    mov w0,w7
+    mov _n,w0
+    dec w0,w0
+    mov w0,_n
+_part_2:;(part_1/_n)------------------------------------------------------------
     mov _n,w4
-    mov #-1,w2
+    cp w4,#1 
+    bra z,_if_w4_is_1
     repeat #17
-    DIVf w2,w4
-    mov w0,w5 ;initializing w5 for MAC in part 3
-;--------------------------part_2:(_x^_n)---------------------------------------
-    mov _x,w0
-    mov w0,_number
-    call _exposant ;result in w12 or w0
-    mov w12,w6 ;initializing w6 for MAC in part 3
-;--------------------------part_3:(mac)-----------------------------------------
-    mac w5*w6,A
-    mov _n,w4
-    inc w4,w4
-    mov w4,_n
-;--------------------------part_4:(reset)---------------------------------------
+    DIVf w7,w4
+    mov w0,w5 ;initializing w5 for MAC in part 4
+_part_3:;(_x^_n)
+   mov _x,w0
+   mov w0,_number
+   call _exposant ;result in w12 or w0
+   mov w12,w6 ;initializing w6 for MAC in part 4
+_part_4:;(mac)
+   mac w5*w6,A
+   mov _n,w4
+   inc w4,w4
+   mov w4,_n
 _exit_ln:
-    NOP
+  
+   NOP
    sftac A,#-15
-    return
+   return
 ;-------------------------------------------------------------------------------
-_test:
-    
-    return
+_if_w4_is_1:
+    mov _x,w6
+    LAC w6,A
+     mov _n,w4
+   inc w4,w4
+   mov w4,_n
+    ;sftac A,#-15
+    bra _exit_ln
+
+     
+      
 ;-------------------------------------------------------------------------------
+_minus_1_exponent_n:
+    mov _n,w3
+    mov #2,w2
+    repeat #17
+    div.s w3,w2
+    cp0 w1
+    bra z,_if_n_is_pair
+    mov #-1,w0
+    return
+_if_n_is_pair:
+    mov #1 ,w0
+    return
+   
+
+;-------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
+;===============================================================================
+;Subroutine _ln_1_PLUS_x initialization section: define the diferent variables 
+;values here and the accumulator configuration
+;..............................................................................
 _INIT_LN:
   BCLR CORCON,#IF
   BSET CORCON,#SATA
   BSET CORCON,#ACCSAT
-  
   clr A
-  
+  ;initializing _n
   mov #1,w0
   mov w0,_n
-  
+  ;initializing _x
   mov #2,w0
   mov w0,_x
-  
-  mov #3,w0
+  ;initializing _N(numbers of iteration of our loop)
+  mov #4,w0
   mov w0,_N
   
   return
 
-;--------End of All Code Sections ---------------------------------------------
+;--------End of All Code Sections ----------------------------------------------
 
 .end                               ;End of program code in this file
 
